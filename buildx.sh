625822d7e88e3e55ab9d693add4502ddbf1bd03a
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-23 21:28:06 +0800

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-sniVer="${1:-0.7.0}"
-dnsVer="${2:-2.92}"
+oneVer="${1:-0.7.0}"
+twoVer="${2:-2.92}"
 withLatest="${3:-0}"
 dockerProject="sniproxy"
 dockerBase="alpine:3.20"
@@ -14,21 +14,21 @@ for arch in "amd64" "arm64"; do
   docker run --privileged --rm tonistiigi/binfmt --install "${arch}"
   docker rm -f "${dockerName}" >/dev/null 2>&1 || true
   docker run --platform "linux/${arch}" --name "${dockerName}" -id -v /mnt:/mnt "${dockerBase}"
-  docker exec "${dockerName}" /bin/sh /mnt/commit.sh "${sniVer}" "${dnsVer}"
-  docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "${dockerProject}_${arch}:${sniVer}"
+  docker exec "${dockerName}" /bin/sh /mnt/commit.sh "${oneVer}" "${twoVer}"
+  docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "${dockerProject}_${arch}:${oneVer}"
   docker rm -f "${dockerName}" >/dev/null 2>&1 || true
   userName="$(docker info 2>/dev/null |grep 'Username:' |cut -d':' -f2 |sed 's/[[:space:]]//g')"
   [ -n "$userName" ] || continue
   [ "${withLatest}" = "1" ] && {
-    docker tag "${dockerProject}_${arch}:${sniVer}" "${userName}/${dockerProject}_${arch}:latest"
+    docker tag "${dockerProject}_${arch}:${oneVer}" "${userName}/${dockerProject}_${arch}:latest"
     docker push "${userName}/${dockerProject}_${arch}:latest"
   }
-  docker tag "${dockerProject}_${arch}:${sniVer}" "${userName}/${dockerProject}_${arch}:${sniVer}"
-  docker push "${userName}/${dockerProject}_${arch}:${sniVer}"
-  [ $? -eq 0 ] && manifest=`echo "--amend \"${userName}/${dockerProject}_${arch}:${sniVer}\" ${manifest}" |sed 's/\ \+$//'`
+  docker tag "${dockerProject}_${arch}:${oneVer}" "${userName}/${dockerProject}_${arch}:${oneVer}"
+  docker push "${userName}/${dockerProject}_${arch}:${oneVer}"
+  [ $? -eq 0 ] && manifest=`echo "--amend \"${userName}/${dockerProject}_${arch}:${oneVer}\" ${manifest}" |sed 's/\ \+$//'`
 done
 
-[ -n "$userName" ] && [ -n "$manifest" ] && eval `echo "docker manifest create \"${userName}/${dockerProject}:${sniVer}\" $manifest"` && docker manifest push -p "${userName}/${dockerProject}:${sniVer}"
+[ -n "$userName" ] && [ -n "$manifest" ] && eval `echo "docker manifest create \"${userName}/${dockerProject}:${oneVer}\" $manifest"` && docker manifest push -p "${userName}/${dockerProject}:${oneVer}"
 [ -n "$userName" ] && [ -n "$manifest" ] && [ "${withLatest}" = "1" ] && eval `echo "docker manifest create \"${userName}/${dockerProject}:latest\" $manifest"` && docker manifest push -p "${userName}/${dockerProject}:latest"
 
 
