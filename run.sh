d026b99558b0a7a1920342f0a47a9112c20ecd33
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-23 18:37:35 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,9 @@ addr=`wget -qO- https://checkip.amazonaws.com/`
 [ -n "$addr" ] || addr=`ip -4 addr show "$net" | awk '/inet /{print $2}' | cut -d/ -f1`
 [ -n "$addr" ] && echo "Addr: ${addr}"
 
+/usr/sbin/dnsmasq -v
+[ -f /etc/sniproxy/dnsmasq-lo.conf ] && /usr/sbin/dnsmasq -C /etc/sniproxy/dnsmasq-lo.conf
+[ -f /etc/sniproxy/dnsmasq-up.conf ] && /usr/sbin/dnsmasq -C /etc/sniproxy/dnsmasq-up.conf
+/usr/sbin/sniproxy -V
 echo `printenv TABLE`
 
