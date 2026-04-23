61cfa542d431251068867082de18149bfa082cdb
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-23 19:27:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# [ ! -e "/dev/net/tun" ] && echo "try with --privileged " && exit 1
 device=`ls -1 /sys/class/net| grep -v '^lo$' |head -n 1`
 [ -n "$device" ] || exit 1
 addr=`printenv ADDR`
