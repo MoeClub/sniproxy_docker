c4c9764e8c735c4a34f346c57cf2d66ec6ffda44
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-23 18:29:09 +0800

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -4,8 +4,10 @@ set -e
 sniVer="${1:-0.7.0}"
 dnsVer="${2:-2.92}"
 withLatest="${3:-0}"
+dockerProject="sniproxy"
 dockerBase="alpine:3.20"
-dockerName="sniproxy_buildx"
+dockerName="${dockerProject}_buildx"
+
 
 manifest=""
 for arch in "amd64" "arm64"; do
@@ -13,21 +15,21 @@ for arch in "amd64" "arm64"; do
   docker rm -f "${dockerName}" >/dev/null 2>&1 || true
   docker run --platform "linux/${arch}" --name "${dockerName}" -id -v /mnt:/mnt "${dockerBase}"
   docker exec "${dockerName}" /bin/sh /mnt/commit.sh "${sniVer}" "${dnsVer}"
-  docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "${arch}:${sniVer}"
+  docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "${dockerProject}_${arch}:${sniVer}"
   docker rm -f "${dockerName}" >/dev/null 2>&1 || true
   userName="$(docker info 2>/dev/null |grep 'Username:' |cut -d':' -f2 |sed 's/[[:space:]]//g')"
   [ -n "$userName" ] || continue
   [ "${withLatest}" = "1" ] && {
-    docker tag "${arch}:${sniVer}" "${userName}/${arch}:latest"
-    docker push "${userName}/${arch}:latest"
+    docker tag "${dockerProject}_${arch}:${sniVer}" "${userName}/${dockerProject}_${arch}:latest"
+    docker push "${userName}/${dockerProject}_${arch}:latest"
   }
-  docker tag "${arch}:${sniVer}" "${userName}/${arch}:${sniVer}"
-  docker push "${userName}/${arch}:${sniVer}"
-  [ $? -eq 0 ] && manifest=`echo "--amend \"${userName}/${arch}:${sniVer}\" ${manifest}" |sed 's/\ \+$//'`
+  docker tag "${dockerProject}_${arch}:${sniVer}" "${userName}/${dockerProject}_${arch}:${sniVer}"
+  docker push "${userName}/${dockerProject}_${arch}:${sniVer}"
+  [ $? -eq 0 ] && manifest=`echo "--amend \"${userName}/${dockerProject}_${arch}:${sniVer}\" ${manifest}" |sed 's/\ \+$//'`
 done
 
-[ -n "$manifest" ] && eval `echo "docker manifest create \"${userName}/ocserv:${sniVer}\" $manifest"` && docker manifest push -p "${userName}/ocserv:${sniVer}"
-[ -n "$manifest" ] && [ "${withLatest}" = "1" ] && eval `echo "docker manifest create \"${userName}/ocserv:latest\" $manifest"` && docker manifest push -p "${userName}/ocserv:latest"
+[ -n "$manifest" ] && eval `echo "docker manifest create \"${userName}/${dockerProject}:${sniVer}\" $manifest"` && docker manifest push -p "${userName}/${dockerProject}:${sniVer}"
+[ -n "$manifest" ] && [ "${withLatest}" = "1" ] && eval `echo "docker manifest create \"${userName}/${dockerProject}:latest\" $manifest"` && docker manifest push -p "${userName}/${dockerProject}:latest"
 
 
 # docker pull ocserv/ocserv:latest
