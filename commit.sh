7c4e01ec392886eab104d11200b2e43ab25fb4c8
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-23 18:22:55 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-ocVer="${1:-1.4.1}"
+sniVer="${1:-0.7.0}"
 dnsVer="${2:-2.92}"
 
 apk add wget iproute2 openssl iptables
-sh /mnt/update.sh "${ocVer}" "${dnsVer}"
-mkdir -p /etc/dnsmasq.d /etc/ocserv/group
-[ -f /mnt/Default ] && cp -rf /mnt/Default /etc/ocserv/group
-[ -f /mnt/NoRoute ] && cp -rf /mnt/NoRoute /etc/ocserv/group
-[ -f /mnt/ocserv.conf ] && cp -rf /mnt/ocserv.conf /etc/ocserv
-[ -f /mnt/p12.sh ] && cp -rf /mnt/p12.sh /etc/ocserv
-[ -f /mnt/dnsmasq.conf ] && cp -rf /mnt/dnsmasq.conf /etc/dnsmasq.conf
+sh /mnt/update.sh "${sniVer}" "${dnsVer}"
+mkdir -p /etc/sniproxy
+[ -f /mnt/sniproxy.conf ] && cp -rf /mnt/sniproxy.conf /etc/sniproxy
+[ -f /mnt/dnsmasq-lo.conf ] && cp -rf /mnt/dnsmasq-lo.conf /etc/sniproxy
+[ -f /mnt/dnsmasq-up.conf ] && cp -rf /mnt/dnsmasq-up.conf /etc/sniproxy
 [ -f /mnt/run.sh ] && cp -rf /mnt/run.sh /run.sh
-chmod -R 777 /etc/dnsmasq.d /etc/dnsmasq.conf /etc/ocserv /run.sh
+chmod -R 777 /etc/sniproxy /run.sh
 find /var -type f -delete
 echo >$HOME/.ash_history
